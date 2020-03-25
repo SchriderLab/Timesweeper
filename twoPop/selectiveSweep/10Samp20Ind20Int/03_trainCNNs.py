cffdeb733f241a87dde2d9d8c244289a33dd1dda
elizabeth <emdavis2@ncsu.edu>
2020-03-25 11:25:58 -0400

    varying number of time points for two pop

---
+++ twoPop/selectiveSweep/10Samp20Ind20Int/03_trainCNNs.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from initializeVar import *
+
+sys.path.insert(1, '/pine/scr/e/m/emae/timeSeriesSweeps')
+
+import runCmdAsJob
+
+prefixLs = ['hard_soft_neut_ttv_ali', 'hard_soft_neut_ttv_haps', 'hard_soft_neut_ttv_sfs', 'hard_soft_neut_ttv_jsfs']
+
+simTypeToScript = {"":"../../keras_CNN_loadNrun.py", "1Samp":"../../keras_DNN_loadNrun.py"}
+for simType in ["", "1Samp"]:
+    outDir="{}/classifiers{}".format(baseDir, simType)
+    os.system("mkdir -p {}".format(outDir))
+
+    for prefix in prefixLs:
+        cmd = "python {0} -i {1}/npzs{2}/{3}.npz -c {4}/{3}.mod".format(simTypeToScript[simType], baseDir, simType, prefix, outDir)
+        runCmdAsJob.runCmdAsJobWithoutWaitingWithLog(cmd, "trainTS", "trainTS.txt", "12:00:00", "general", "32GB", outDir+"/{}.log".format(prefix))
---
+++ twoPop/selectiveSweep/2Samp20Ind100Int/initializeVar.py
@@ -0,0 +1,11 @@
+baseDir="/pine/scr/e/m/emae/data/popGenCnn/timeSeriesSweeps/twoPop/selectiveSweep/2Samp20Ind100Int"
+
+# Time Series
+sampleSizePerStepTS = 20  #individuals in sample for each time interval in time series 
+numSamplesTS = 2  #number of time points sampled in time series
+samplingIntervalTS = 100  #spacing between time points
+
+# 1 Sample at 1 One Time Point
+sampleSizePerStep1Samp = 40 #size of population sampled at one time point so that it is same size as time series data
+numSamples1Samp = 1  #number of time points sampled
+samplingInterval1Samp = 200  #spacing between time points
