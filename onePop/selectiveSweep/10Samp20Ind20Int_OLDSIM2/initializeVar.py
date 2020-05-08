575be75b783e8619853bcab97b8bea404004a51b
elizabeth <emdavis2@ncsu.edu>
2020-05-08 18:09:37 -0400

    randomly choosing mutTime from a range

---
+++ onePop/selectiveSweep/10Samp20Ind20Int_OLDSIM2/initializeVar.py
@@ -0,0 +1,11 @@
+baseDir="/pine/scr/e/m/emae/data/popGenCnn/timeSeriesSweeps/onePop/selectiveSweep/10Samp20Ind20Int"
+
+# Time Series
+sampleSizePerStepTS = 20  #individuals in sample for each time interval in time series 
+numSamplesTS = 10  #number of time points sampled in time series
+samplingIntervalTS = 20  #spacing between time points
+
+# 1 Sample at 1 One Time Point
+sampleSizePerStep1Samp = 200 #size of population sampled at one time point so that it is same size as time series data
+numSamples1Samp = 1  #number of time points sampled
+samplingInterval1Samp = 200  #spacing between time points
