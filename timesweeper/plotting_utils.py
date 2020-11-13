a8fe7bace5f32c8e0d6505d137769009b7462083
Logan Whitehouse <lswhiteh@live.unc.edu>
2020-11-12 19:32:41 -0500

    Plotting utils added.

---
+++ timesweeper/plotting_utils.py
@@ -0,0 +1,144 @@
+import itertools
+import os
+
+import matplotlib
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix
+
+plt.ioff()
+
+
+def plot_confusion_matrix(
+    working_dir, cm, target_names, title="Confusion matrix", cmap=None, normalize=False
+):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap("Blues")
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation="nearest", cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(
+                j,
+                i,
+                "{:0.4f}".format(cm[i, j]),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black",
+            )
+        else:
+            plt.text(
+                j,
+                i,
+                "{:,}".format(cm[i, j]),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black",
+            )
+
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel(
+        "Predicted label\naccuracy={:0.4f}; misclass={:0.4f}".format(accuracy, misclass)
+    )
+
+    plt.savefig(os.path.join(working_dir, title + "_discriminator_conf_matrix.png"))
+
+
+def print_confusion_matrix(y_true, y_pred):
+    """Prints confusion matrix
+
+    Args:
+        y_true (nparray): 1D npy array containing int values for class
+        y_pred (nparray): 1D npy array containing int values for predicted class
+    """
+    conf_mat = confusion_matrix(y_true, y_pred)
+
+    print("Confusion Matrix")
+    print(conf_mat)
+    return conf_mat
+
+
+def plot_training(working_dir, history, model_save_name):
+    """Plots training and validation accuracies
+
+    Args:
+        working_dir (str): Location to save model
+        history (Keras history object): Model history after training and validation
+        model_save_name (str): Name to use for title and name of plot
+    """
+    # Plot accuracy over validation accuracy during training
+    plt.plot(history.history["accuracy"], label="accuracy")
+    plt.plot(history.history["val_accuracy"], label="val_accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.ylim([0, 1])
+    plt.legend(loc="upper left")
+    plt.title(model_save_name)
+
+    imgFile = os.path.join(working_dir, model_save_name + "_training.png")
+    if os.path.isfile(imgFile):
+        os.remove(imgFile)
+
+    plt.savefig(imgFile)
+
+
+def print_classification_report(y_true, y_pred):
+    """Prints classification report
+
+    Args:
+        y_true (nparray): 1D npy array containing int values for class
+        y_pred (nparray): 1D npy array containing int values for predicted class
+        train_gen (Keras Generator): Training generator used for model training, used for labels
+    """
+    print("Classification Report")
+    print(classification_report(y_true, y_pred))
