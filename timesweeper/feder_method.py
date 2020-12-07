6f3e2954b836a6cc1f24ced1aba28d61e3cc7921
Logan Whitehouse <lswhiteh@live.unc.edu>
2020-12-07 18:21:13 -0500

    Added actual sim data for testing and fixed formatting prints.

--- timesweeper/feder_method.py
+++ timesweeper/feder_method.py
@@ -2,6 +2,7 @@ from math import sqrt
 from typing import List, Tuple
 import random as rd
 import sys
+import numpy as np
 
 # https://www.genetics.org/content/196/2/509
 
@@ -39,34 +40,35 @@ def calc_FIT(Ybar: float, S2: float, L: int) -> float:
 
 def gen_test_data(test_seed: int) -> Tuple[List[float], List[int], int]:
     rd.seed(test_seed)
-    max_gen = rd.randint(10, 100)  # Total num time samples
-    test_t_num = rd.randint(3, 10)  # For testing just take evenly-spaced timepoints
-    test_t = [i for i in range(max_gen) if i % test_t_num == 0]
-    test_L = len(test_t)
-    test_v = [
-        rd.random() for i in range(test_L)
-    ]  # Generate frequencies for each timepoint
+    test_L = 10
+    test_t_lim = 1000
+    test_t = [i for i in range(0, test_t_lim + 1) if i % (test_t_lim / test_L) == 0]
+    test_v = list(np.random.normal(loc=0.5, scale=0.1, size=test_L))
 
     return test_v, test_t, test_L
 
 
 def main():
-    v, t, L = gen_test_data(42)
-    print("freqs:", v)
-    print("timepoint samps:", t)
-    print("max gens:", L)
+    # v, t, L = gen_test_data(42)
+    v = [0.06, 0.03, 0.061, 0.091, 0.236, 0.252, 0.345, 0.492, 0.679, 0.818]
+    t = [10060, 10080, 10100, 10120, 10140, 10160, 10180, 10200, 10220, 10240]
+    L = len(t)
+
+    print("\nFreqs:", v)
+    print("\nTimepoint samps:", t)
+    print("\nNumber of timepoints sampled:", L)
 
     Yi = calc_Yi(v, t, L)
-    print("Yi:", Yi)
+    print("\nYi:", Yi)
 
     Ybar = calc_Ybar(Yi, L)
-    print("Ybar:", Ybar)
+    print("\nYbar:", Ybar)
 
     S2 = calc_S2(Yi, Ybar, L)
-    print("S2:", S2)
+    print("\nS2:", S2)
 
     FIT = calc_FIT(Ybar, S2, L)
-    print("FIT Value:", FIT)
+    print("\nFIT Value:", FIT)
 
 
 if __name__ == "__main__":
