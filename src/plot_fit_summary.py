37ef75357c8e7f311bece65ec7dba90c1dbe6a2c
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-01-22 19:07:58 -0500

    CNN results now read in to plot ROC too.

--- src/plot_fit_summary.py
+++ src/plot_fit_summary.py
@@ -7,28 +7,26 @@ fit_df = pd.read_csv(
     "/pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-selectiveSweep-20Samp-10Int/sample_dict.csv"
 )
 
+cnn_df = pd.read_csv(
+    "/pine/scr/l/s/lswhiteh/timeSeriesSweeps/cnn_preds_rocformat.csv", header=0
+)
 
 plt.figure()
 plt.title("FIt ROC - All Types")
 plt.xlabel("False Positive Rate")
 plt.ylabel("True Postitive Rate")
 
-fpr, tpr, thresh = roc_curve(fit_df["true_site_soft"], fit_df["min_p_soft"])
-auc = roc_auc_score(fit_df["true_site_soft"], fit_df["min_p_soft"])
-plt.plot(fpr, tpr, label="Min p val soft, auc=" + str(auc))
 
-fpr, tpr, thresh = roc_curve(fit_df["true_site_hard"], fit_df["min_p_hard"])
-auc = roc_auc_score(fit_df["true_site_hard"], fit_df["min_p_hard"])
-plt.plot(fpr, tpr, label="Min p val hard, auc=" + str(auc))
+fpr, tpr, thresh = roc_curve(cnn_df["sweep_present"], cnn_df["sweep_predicted"])
+print(thresh)
+auc = roc_auc_score(cnn_df["sweep_present"], cnn_df["sweep_predicted"])
+plt.plot(fpr, tpr, label="CNN Preds, auc=" + str(auc))
 
-fpr, tpr, thresh = roc_curve(fit_df["true_site_soft"], fit_df["mean_detect_soft"])
-auc = roc_auc_score(fit_df["true_site_soft"], fit_df["mean_detect_soft"])
-plt.plot(fpr, tpr, label="Mean p val soft, auc=" + str(auc))
+fpr, tpr, thresh = roc_curve(fit_df["sweep_present"], fit_df["sweep_predicted"])
+print(thresh)
+auc = roc_auc_score(fit_df["sweep_present"], fit_df["sweep_predicted"])
+plt.plot(fpr, tpr, label="Min p val FIt, auc=" + str(auc))
 
-fpr, tpr, thresh = roc_curve(fit_df["true_site_hard"], fit_df["mean_detect_hard"])
-auc = roc_auc_score(fit_df["true_site_hard"], fit_df["mean_detect_hard"])
-plt.plot(fpr, tpr, label="Mean p val hard, auc=" + str(auc))
-
-plt.legend(loc="bottom right")
+plt.legend(loc="lower right")
 
 plt.savefig("fits_rocs.png")
