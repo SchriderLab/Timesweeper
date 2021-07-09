f4c1f216e8d8ab99cdb3ea01ef67b08033725ea4
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-07-09 15:48:16 -0400

    Jobfile updates.

--- src/jobfiles/run_jobs.sh
+++ src/jobfiles/run_jobs.sh
@@ -1,12 +1,17 @@
 for i in \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-selectiveSweep-10Samp-20Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-selectiveSweep-5Samp-40Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-selectiveSweep-2Samp-100Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-selectiveSweep-20Samp-10Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-selectiveSweep-40Samp-5Int
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-10Samp-20Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-5Samp-40Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-2Samp-100Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-20Samp-10Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-40Samp-5Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-10Samp-20Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-5Samp-40Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-2Samp-100Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-20Samp-10Int \
+    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-40Samp-5Int
 do
-    python ../hap_networks.py prep ${i}
-    python ../hap_networks.py prep ${i} --time-series
+    #python ../hap_networks.py prep ${i}
+    #python ../hap_networks.py prep ${i} --time-series
 
     sbatch train_hap_net.sb $i
     sbatch train_hap_net.sb $i --time-series
