982cc308c40105a4697c6d853a9a2375095a62ec
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-07-27 12:59:48 -0400

    NPZ merging now a separate script for easier SLURM allocation.

---
+++ src/merge_npzs.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tqdm import tqdm
+import sys
+
+outfile = sys.argv[1]
+filelist = sys.argv[2:]
+
+# Collect all NPZ entries into a single file for the entire training dataset
+data_all = []
+for fname in tqdm(filelist, desc="Loading NPZ files for merging"):
+    data_all.append(np.load(fname))
+merged_data = {}
+for data in tqdm(data_all, desc="Merging npz files"):
+    for k, v in data.items():
+        merged_data.update({k: v})
+np.savez(outfile, **merged_data)
