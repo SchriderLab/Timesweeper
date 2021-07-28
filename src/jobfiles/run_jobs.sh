09412382edc55ba8885c3e615023549fb397a608
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-07-27 20:55:39 -0400

    Workflow now only runs one job per simetype per schema to reduce longleaf usage.
    Other misc changes to configs to reflect this.

--- src/jobfiles/run_jobs.sh
+++ src/jobfiles/run_jobs.sh
@@ -1,18 +1,4 @@
-for i in \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-10Samp-20Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-5Samp-40Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-2Samp-100Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-20Samp-10Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/onePop-adaptiveIntrogression-40Samp-5Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-10Samp-20Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-5Samp-40Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-2Samp-100Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-20Samp-10Int \
-    /pine/scr/l/s/lswhiteh/timeSeriesSweeps/twoPop-adaptiveIntrogression-40Samp-5Int
-do
-    #python ../hap_networks.py prep ${i}
-    #python ../hap_networks.py prep ${i} --time-series
-
-    sbatch train_hap_net.sb $i
-    sbatch train_hap_net.sb $i --time-series
-done
+for i in /overflow/dschridelab/users/lswhiteh/timeSeriesSweeps/src/run_configs/uniform_vs_dense_schema_comparison/*
+    do
+        sbatch run_snakemake.sb $i 
+    done
