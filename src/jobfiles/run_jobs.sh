bd0ca1fc721d755a17f60e30ed0032e4fefffc2c
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-09-20 10:53:50 -0400

    Configs for post-sweep and selection runs.

--- src/jobfiles/run_jobs.sh
+++ src/jobfiles/run_jobs.sh
@@ -1,4 +1,4 @@
-for i in /overflow/dschridelab/users/lswhiteh/timeSeriesSweeps/src/run_configs/sel_coeff/*
+for i in 400 600 800 1000 
     do
-        sbatch run_snakemake.sb $i 
+        sbatch run_snakemake.sb /pine/scr/l/s/lswhiteh/timeSeriesSweeps-code/src/run_configs/post_selection_timing/uniform_10pt_20samp_${i}gens_post_sweep.yaml
     done
