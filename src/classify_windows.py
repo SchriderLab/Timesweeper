f9dae98724eb1ce58558a4277338557b3e5dd045
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-12-14 11:19:32 -0500

    Window scanning implemented with relevant util modules.

--- src/classify_windows.py
+++ src/classify_windows.py
@@ -1,31 +1,96 @@
-import os, sys
-import multiprocessing as mp
 import argparse as ap
-from utils.vcf import vcf_to_afs, create_afm
+import multiprocessing as mp
+import os
 
-# from tensorflow.keras.models import load_model
+import allel
 import numpy as np
+import pandas as pd
+from tensorflow.keras.models import load_model
+from tqdm import tqdm
+
+from feder_fit import fit
+from utils.haps import getTSHapFreqs, haps_to_strlist
+from utils.vcf import vcf_to_genos, vcf_to_haps
 
 
-def run_windows(snps, afs, model):
+def run_afs_windows(snps, genos, samp_sizes, win_size, model):
+    ts_genos = split_arr(genos, samp_sizes)
+    afs = np.stack([calc_mafs(allel.GenotypeArray(geno)) for geno in ts_genos])
+    padded_freqs = pad_arr(afs, win_size)
     results_dict = {}
-    for center in range(len(snps)):
-        win_idxs = get_window_idxs(center)
-        window = np.swapaxes(afs[win_idxs, :], 0, 1)
-        probs = model.predict(window)
+    for center in tqdm(range(len(snps)), desc="Predicting on AFS windows"):
+        win_idxs = get_window_idxs(center, win_size)
+        window = padded_freqs[:, win_idxs]
+
+        probs = model.predict(np.expand_dims(window, 0))
         results_dict[snps[center]] = probs
 
+    return results_dict
 
-def pad_neg_idxs(idxs, arr):
-    pass
+
+def run_fit_windows(snps, genos, samp_sizes, gens):
+    ts_genos = split_arr(genos, samp_sizes)
+    afs = np.stack([calc_mafs(allel.GenotypeArray(geno)) for geno in ts_genos])
+    results_dict = {}
+    for idx in tqdm(range(len(snps)), desc="Calculating FIT values"):
+        results_dict[snps[idx]] = fit(list(afs[:, idx]), gens)  # tval, pval
+
+    return results_dict
+
+
+def run_hfs_windows(snps, haps, samp_sizes, win_size, model):
+    padded_haps = pad_arr(np.swapaxes(haps, 0, 1), win_size)
+    results_dict = {}
+    for center in tqdm(range(len(snps)), desc="Predicting on HFS windows"):
+        win_idxs = get_window_idxs(center, win_size)
+        window = padded_haps[:, win_idxs]
+        str_window = haps_to_strlist(np.swapaxes(window, 0, 1))
+        hfs = getTSHapFreqs(str_window, samp_sizes)
+
+        probs = model.predict(np.expand_dims(hfs, 0))
+        results_dict[snps[center]] = probs
+
+    return results_dict
+
+
+def pad_arr(arr, win_size):
+    """Puts afs in the middle of zeros size of window on either side."""
+    padded = np.zeros((arr.shape[0], (2 * win_size) + arr.shape[1]))
+    padded[:, win_size : win_size + arr.shape[1]] = arr
+    return padded
+
+
+def split_arr(arr, samp_sizes):
+    i = 0
+    arr_list = []
+    for j in samp_sizes:
+        arr_list.append(arr[:, i : i + j])
+        i += j
+
+    return np.stack(arr_list)
+
+
+def calc_mafs(geno_arr):
+    alleles = geno_arr.count_alleles()
+    return alleles[:, 1] / alleles.sum(axis=1)
+
+
+### Merge timepoints
+def dicts_to_keyset(freq_dicts):
+    keys = []
+    for i in freq_dicts:
+        keys.extend([*i])
+
+    return list(set(keys))
 
 
 def classify_window(win_snps, model):
     return model.predict(win_snps)
 
 
-def get_window_idxs(center_idx):
-    return list(range(center_idx - 25, center_idx + 25 + 1))
+def get_window_idxs(center_idx, win_size):
+    half_window = int(win_size / 2)
+    return list(range(center_idx - half_window, center_idx + half_window + 1))
 
 
 def load_nn(model_path, summary=False):
@@ -36,10 +101,45 @@ def load_nn(model_path, summary=False):
     return model
 
 
-def worker(args):
-    vcfs, model = args
-    freq_dicts = [vcf_to_afs(vcf_file) for vcf_file in vcfs]
-    snps, afs = create_afm(freq_dicts)
+def write_fit(fit_dict, outfile):
+    chroms, bps = zip(*fit_dict.keys())
+
+    inv_pval = [1 - i[1] for i in fit_dict.values()]
+
+    predictions = pd.DataFrame({"Chrom": chroms, "BP": bps, "Inv pval": inv_pval})
+    predictions.sort_values(["Chrom", "BP"], inplace=True)
+
+    predictions.to_csv(os.path.join(outfile), header=True, index=False, sep="\t")
+
+
+def write_preds(pred_dict, outfile):
+    lab_dict = {0: "Neut", 1: "Hard", 2: "Soft"}
+    chroms, bps = zip(*pred_dict.keys())
+
+    neut_scores = [i[0][0] for i in pred_dict.values()]
+    hard_scores = [i[0][1] for i in pred_dict.values()]
+    soft_scores = [i[0][2] for i in pred_dict.values()]
+
+    classes = [lab_dict[np.argmax(i, axis=1)[0]] for i in pred_dict.values()]
+    predictions = pd.DataFrame(
+        {
+            "Chrom": chroms,
+            "BP": bps,
+            "Class": classes,
+            "Neut Score": neut_scores,
+            "Hard Score": hard_scores,
+            "Soft Score": soft_scores,
+        }
+    )
+    predictions.sort_values(["Chrom", "BP"], inplace=True)
+
+    predictions.to_csv(os.path.join(outfile), header=True, index=False, sep="\t")
+
+
+def add_file_label(filename, label):
+    splitfile = filename.split(".")
+    newname = f"{''.join(splitfile[:-1])}_{label}.{splitfile[-1]}"
+    return newname
 
 
 def parse_ua():
@@ -48,48 +148,76 @@ def parse_ua():
     )
     uap.add_argument(
         "-i",
-        "--input-vcfs",
+        "--input-vcf",
         dest="input_vcfs",
-        help="VCFs to scan for sweeps. Provide in order of earliest to most recent samples.",
+        help="Merged VCF to scan for sweeps. Must be merged VCF where files are merged in order from earliest to latest sampling time, -0 flag must be used.",
+        required=True,
+    )
+
+    uap.add_argument(
+        "-s",
+        "--sample-sizes",
+        dest="samp_sizes",
+        help="Number of diploid individuals from each timepoint sampled. Used to index VCF data from earliest to latest sampling points.",
+        required=True,
         nargs="+",
+    )
+    uap.add_argument(
+        "--afs-model",
+        dest="afs_model",
+        help="Path to Keras2-style saved model to load for AFS prediction.",
+        required=True,
+    )
+    uap.add_argument(
+        "--hfs-model",
+        dest="hfs_model",
+        help="Path to Keras2-style saved model to load for HFS prediction.",
         required=True,
     )
     uap.add_argument(
         "-o",
-        "--output-dir",
-        dest="output_dir",
-        help="Directory to write results to.",
+        "--output",
+        dest="outfile",
+        help="File to write results to.",
         required=False,
-        default=os.getcwd(),
+        default="Timesweeper_predictions.csv",
     )
-    uap.add_argument(
-        "-nn",
-        "--network-model",
-        dest="nn_model",
-        help="Path to Keras2-style saved model to load for prediction.",
-        default=None,
-    )
-    uap.add_argument(
-        "--threads",
-        dest="threads",
-        help="Threads to use for multiprocessing.",
-        default=mp.cpu_count() - 1 or 1,
-    )
-
     return uap.parse_args()
 
 
 def main():
     ua = parse_ua()
-    input_vcfs, output_dir, nn_model, threads = (
+    input_vcf, samp_sizes, outfile, afs_model, hfs_model = (
         ua.input_vcfs,
-        ua.output_dir,
-        ua.nn_model,
-        ua.threads,
+        [int(i) for i in ua.samp_sizes],
+        ua.outfile,
+        load_nn(ua.afs_model),
+        load_nn(ua.hfs_model),
     )
-    model = load_nn(nn_model)
-    worker(input_vcfs)
+    win_size = 51  # Must be consistent with training data
 
+    # AFS
+    genos, snps = vcf_to_genos(input_vcf)
+    predictions = run_afs_windows(snps, genos, samp_sizes, win_size, afs_model)
+    afs_file = add_file_label(outfile, "afs")
+    write_preds(predictions, afs_file)
+
+    # FIT
+    GEN_STEP = 10
+    GENS = list(range(10060, 10250 + GEN_STEP, GEN_STEP))
+    genos, snps = vcf_to_genos(input_vcf)
+    predictions = run_fit_windows(snps, genos, samp_sizes, GENS)
+    fit_file = add_file_label(outfile, "fit")
+    write_fit(predictions, fit_file)
+
+    # HFS
+    haps, snps = vcf_to_haps(input_vcf)
+    predictions = run_hfs_windows(snps, haps, samp_sizes, win_size, hfs_model)
+    hfs_file = add_file_label(outfile, "hfs")
+    write_preds(predictions, hfs_file)
+
+
+# python classify_windows.py -nn ../simple_sims/models/bighaps_TimeSweeper -i ../simple_sims/vcf_sims/hard/pops/1/merged.vcf.gz -s $(printf '10 %.s' {1..20})
 
 if __name__ == "__main__":
     main()
--- src/utils/vcf.py
+++ src/utils/vcf.py
@@ -1,17 +1,7 @@
 import allel
 import numpy as np
 
-### Get afs from vcf
-def vcf_to_afs(vcf_file):
-    vcf = read_vcf(vcf_file)
-    geno_arr = get_geno_arr(vcf)
-    mafs = calc_mafs(geno_arr)
-    locs = make_loc_tups(vcf)
-    freqs_dict = annotate_counts(mafs, locs)
-
-    return freqs_dict
-
-
+# General util functions
 def read_vcf(vcf_file):
     vcf = allel.read_vcf(
         vcf_file, fields=["variants/CHROM", "variants/POS", "calldata/GT"]
@@ -24,38 +14,24 @@ def get_geno_arr(vcf):
     return allel.GenotypeArray(vcf["calldata/GT"])
 
 
-def calc_mafs(geno_arr):
-    return geno_arr.count_alleles()[:, 1] / geno_arr.count_alleles().sum(axis=1)
-
-
 def make_loc_tups(vcf):
-    return zip(vcf["variants/CHROM"], vcf["variants/POS"])
+    return list(zip(vcf["variants/CHROM"], vcf["variants/POS"]))
 
 
-def annotate_counts(minor_allele_counts, loc_keys):
-    """Generates dict in the form of {(chrom, bp): freq}"""
-    return dict(zip(loc_keys, minor_allele_counts))
+### Get afs from vcf
+def vcf_to_genos(vcf_file):
+    vcf = read_vcf(vcf_file)
+    geno_arr = get_geno_arr(vcf)
+    locs = make_loc_tups(vcf)
+
+    return geno_arr, locs
 
 
-### Merge timepoints
-def dicts_to_keyset(freq_dicts):
-    keys = []
-    for i in freq_dicts:
-        keys.extend([*i])
+### Haplotypes from VCF
+def vcf_to_haps(vcf_file):
+    vcf = read_vcf(vcf_file)
+    hap_arr = get_geno_arr(vcf).to_haplotypes()
+    locs = make_loc_tups(vcf)
 
-    return set(keys)
+    return hap_arr, locs
 
-
-def create_afm(freq_dicts):
-    snps = dicts_to_keyset(freq_dicts)
-    afs = []
-    for snp in snps:
-        _freqs = []
-        for sample in freq_dicts:
-            if snp in sample:
-                _freqs.append(sample[snp])
-            else:
-                _freqs.append(0.0)
-        afs.append(_freqs)
-
-    return snps, np.array(afs)
