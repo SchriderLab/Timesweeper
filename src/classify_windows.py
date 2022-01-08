9f39ff301c3577830585b8421ccba90f2ac5222b
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-01-08 09:59:06 -0500

    Buffering and mut type reporting implemented.

--- src/classify_windows.py
+++ src/classify_windows.py
@@ -16,11 +16,13 @@ from utils.vcf import vcf_to_genos, vcf_to_haps
 def run_afs_windows(snps, genos, samp_sizes, win_size, model):
     ts_genos = split_arr(genos, samp_sizes)
     afs = np.stack([calc_mafs(allel.GenotypeArray(geno)) for geno in ts_genos])
-    padded_freqs = pad_arr(afs, win_size)
     results_dict = {}
-    for center in tqdm(range(len(snps)), desc="Predicting on AFS windows"):
+    buffer = int(win_size / 2)
+    for center in tqdm(
+        range(buffer, len(snps) - buffer), desc="Predicting on AFS windows"
+    ):
         win_idxs = get_window_idxs(center, win_size)
-        window = padded_freqs[:, win_idxs]
+        window = afs[:, win_idxs]
 
         probs = model.predict(np.expand_dims(window, 0))
         results_dict[snps[center]] = probs
@@ -28,36 +30,31 @@ def run_afs_windows(snps, genos, samp_sizes, win_size, model):
     return results_dict
 
 
-def run_fit_windows(snps, genos, samp_sizes, gens):
-    ts_genos = split_arr(genos, samp_sizes)
-    afs = np.stack([calc_mafs(allel.GenotypeArray(geno)) for geno in ts_genos])
-    results_dict = {}
-    for idx in tqdm(range(len(snps)), desc="Calculating FIT values"):
-        results_dict[snps[idx]] = fit(list(afs[:, idx]), gens)  # tval, pval
-
-    return results_dict
-
-
 def run_hfs_windows(snps, haps, samp_sizes, win_size, model):
-    padded_haps = pad_arr(np.swapaxes(haps, 0, 1), win_size)
     results_dict = {}
-    for center in tqdm(range(len(snps)), desc="Predicting on HFS windows"):
+    buffer = int(win_size / 2)
+    for center in tqdm(
+        range(buffer, len(snps) - buffer), desc="Predicting on HFS windows"
+    ):
         win_idxs = get_window_idxs(center, win_size)
-        window = padded_haps[:, win_idxs]
-        str_window = haps_to_strlist(np.swapaxes(window, 0, 1))
+        window = np.swapaxes(haps[win_idxs, :], 0, 1)
+        str_window = haps_to_strlist(window)
         hfs = getTSHapFreqs(str_window, samp_sizes)
-
         probs = model.predict(np.expand_dims(hfs, 0))
         results_dict[snps[center]] = probs
 
     return results_dict
 
 
-def pad_arr(arr, win_size):
-    """Puts afs in the middle of zeros size of window on either side."""
-    padded = np.zeros((arr.shape[0], (2 * win_size) + arr.shape[1]))
-    padded[:, win_size : win_size + arr.shape[1]] = arr
-    return padded
+def run_fit_windows(snps, genos, samp_sizes, win_size, gens):
+    ts_genos = split_arr(genos, samp_sizes)
+    afs = np.stack([calc_mafs(allel.GenotypeArray(geno)) for geno in ts_genos])
+    results_dict = {}
+    buffer = int(win_size / 2)
+    for idx in tqdm(range(buffer, len(snps) - buffer), desc="Calculating FIT values"):
+        results_dict[snps[idx]] = fit(list(afs[:, idx]), gens)  # tval, pval
+
+    return results_dict
 
 
 def split_arr(arr, samp_sizes):
@@ -102,29 +99,32 @@ def load_nn(model_path, summary=False):
 
 
 def write_fit(fit_dict, outfile):
-    chroms, bps = zip(*fit_dict.keys())
+    chroms, bps, mut_type = zip(*fit_dict.keys())
 
     inv_pval = [1 - i[1] for i in fit_dict.values()]
 
-    predictions = pd.DataFrame({"Chrom": chroms, "BP": bps, "Inv pval": inv_pval})
+    predictions = pd.DataFrame(
+        {"Chrom": chroms, "BP": bps, "Mut Type": mut_type, "Inv pval": inv_pval}
+    )
     predictions.sort_values(["Chrom", "BP"], inplace=True)
 
     predictions.to_csv(os.path.join(outfile), header=True, index=False, sep="\t")
 
 
-def write_preds(pred_dict, outfile):
+def write_preds(results_dict, outfile):
     lab_dict = {0: "Neut", 1: "Hard", 2: "Soft"}
-    chroms, bps = zip(*pred_dict.keys())
+    chroms, bps, mut_type = zip(*results_dict.keys())
 
-    neut_scores = [i[0][0] for i in pred_dict.values()]
-    hard_scores = [i[0][1] for i in pred_dict.values()]
-    soft_scores = [i[0][2] for i in pred_dict.values()]
+    neut_scores = [i[0][0] for i in results_dict.values()]
+    hard_scores = [i[0][1] for i in results_dict.values()]
+    soft_scores = [i[0][2] for i in results_dict.values()]
 
-    classes = [lab_dict[np.argmax(i, axis=1)[0]] for i in pred_dict.values()]
+    classes = [lab_dict[np.argmax(i, axis=1)[0]] for i in results_dict.values()]
     predictions = pd.DataFrame(
         {
             "Chrom": chroms,
             "BP": bps,
+            "Mut Type": mut_type,
             "Class": classes,
             "Neut Score": neut_scores,
             "Hard Score": hard_scores,
@@ -149,7 +149,7 @@ def parse_ua():
     uap.add_argument(
         "-i",
         "--input-vcf",
-        dest="input_vcfs",
+        dest="input_vcf",
         help="Merged VCF to scan for sweeps. Must be merged VCF where files are merged in order from earliest to latest sampling time, -0 flag must be used.",
         required=True,
     )
@@ -188,7 +188,7 @@ def parse_ua():
 def main():
     ua = parse_ua()
     input_vcf, samp_sizes, outfile, afs_model, hfs_model = (
-        ua.input_vcfs,
+        ua.input_vcf,
         [int(i) for i in ua.samp_sizes],
         ua.outfile,
         load_nn(ua.afs_model),
@@ -196,18 +196,19 @@ def main():
     )
     win_size = 51  # Must be consistent with training data
 
+    
     # AFS
     genos, snps = vcf_to_genos(input_vcf)
     predictions = run_afs_windows(snps, genos, samp_sizes, win_size, afs_model)
-    afs_file = add_file_label(outfile, "afs")
-    write_preds(predictions, afs_file)
+    # afs_file = add_file_label(outfile, "afs")
+    # write_preds(predictions, afs_file)
 
     # FIT
-    GEN_STEP = 10
-    GENS = list(range(10060, 10250 + GEN_STEP, GEN_STEP))
-    genos, snps = vcf_to_genos(input_vcf)
-    predictions = run_fit_windows(snps, genos, samp_sizes, GENS)
-    fit_file = add_file_label(outfile, "fit")
+    # GEN_STEP = 10
+    # GENS = list(range(10060, 10250 + GEN_STEP, GEN_STEP))
+    # genos, snps = vcf_to_genos(input_vcf)
+    # predictions = run_fit_windows(snps, genos, samp_sizes, win_size, GENS)
+    # fit_file = add_file_label(outfile, "fit")
     write_fit(predictions, fit_file)
 
     # HFS
