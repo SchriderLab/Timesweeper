44bbc304cbd78650935a938bca3b2893f7e1f23f
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-02-28 22:32:16 -0500

    Created a editable script for simulating from a non-stdpopsim SLiM model.

---
+++ src/simple_simulate.py
@@ -0,0 +1,151 @@
+import argparse
+import logging
+import multiprocessing as mp
+import os
+import subprocess
+
+import numpy as np
+
+
+def make_d_block(sweep, outFile, dumpfile, verbose=False):
+    """
+    This is meant to be a very customizeable block of text for adding custom args to SLiM as constants.
+    Can add other functions to this module and call them here e.g. pulling selection coeff from a dist.
+    This block MUST INCLUDE the 'sweep' and 'outFile' params, and at the very least the outFile must be used as output for outputVCFSample.
+    Please note that when feeding strings as a constant you must escape them since this is a shell process.
+    """
+
+    num_sample_points = 20
+    inds_per_tp = 5  # Diploid inds
+    physLen = 500000
+
+    d_block = f"""
+    -d sweep=\"{sweep}\" \
+    -d outFile=\"{outFile}\" \
+    -d dumpFile=\"{dumpfile}\" \
+    -d samplingInterval={200/num_sample_points} \
+    -d numSamples={num_sample_points} \
+    -d sampleSizePerStep={inds_per_tp} \
+    -d physLen=${physLen} \
+    -d seed={np.random.randint(0, 1e16)} \
+    """
+    if verbose:
+        logging.info(f"Using the following constants with SLiM: {d_block}")
+
+    return d_block
+
+
+def run_slim(slimfile, slim_path, d_block):
+    subprocess.run(f"{slim_path} {d_block} {slimfile}")
+
+
+def get_ua():
+    agp = argparse.ArgumentParser(
+        description="Simulates selection for training Timesweeper using a pre-made SLiM script."
+    )
+
+    agp.add_argument(
+        "-i",
+        "--slim-file",
+        required=True,
+        type=str,
+        help="SLiM Script to simulate with. Must output to a single VCF file. ",
+        dest="slim_file",
+    )
+    agp.add_argument(
+        "--reps",
+        required=True,
+        type=int,
+        help="Number of replicate simulations to run.",
+        dest="reps",
+    )
+    agp.add_argument(
+        "--work-dir",
+        required=False,
+        type=str,
+        default="./ts_experiment",
+        dest="work_dir",
+        help="Directory to start workflow in, subdirs will be created to write simulation files to. Will be used in downstream processing as well.",
+    )
+    agp.add_argument(
+        "--slim-path",
+        required=False,
+        type=str,
+        default="./SLiM/build/slim",
+        dest="slim_path",
+        help="Path to SLiM executable.",
+    )
+    agp.add_argument(
+        "--threads",
+        required=False,
+        type=int,
+        default=mp.cpu_count(),
+        dest="threads",
+        help="Number of processes to parallelize across.",
+    )
+    return agp.parse_args()
+
+
+def main():
+    """
+    For simulating non-stdpopsim SLiMfiles.
+    Currently only works with 1 pop models where m2 is the sweep mutation.
+    Otherwise everything else is identical to stdpopsim version, just less complex.
+
+    Generalized block of '-d' arguments to give to SLiM at the command line allow for 
+    flexible script writing within the context of this wrapper. If you write your SLiM script
+    to require args set at runtime, this should be easily modifiable to do what you need and 
+    get consistent results to plug into the rest of the workflow.
+
+    The two things you *will* need to specify in your '-d' args to SLiM (and somewhere in the slim script) are:
+    - sweep [str] One of "neut", "hard", or "soft". If you're testing only a neut/hard model, 
+        make the soft a dummy switch for the neutral scenario.
+    - outFile [path] You will need to define this as a population outputVCFSample input, with replace=T and append=T. 
+        This does *not* need to be specified by you in the custom -d block, it will be standardized to work with the rest of the pipeline using work_dir.
+        example line for slim script: `p1.outputVCFSample(sampleSizePerStep, replace=F, append=T, filePath=outFile);`
+        
+
+    """
+    ua = get_ua()
+
+    work_dir = ua.work_dir
+    vcf_dir = f"{work_dir}/vcfs"
+    dumpfile_dir = f"{work_dir}/dumpfiles"
+
+    sweeps = ["neut", "hard", "soft"]
+
+    for i in [vcf_dir, dumpfile_dir]:
+        for sweep in sweeps:
+            os.makedirs(f"{i}/{sweep}", exist_ok=True)
+
+    with mp.Pool(processes=ua.threads) as pool:
+        # Inject info into SLiM script and then simulate, store params for reproducibility
+        sim_params = []
+        for sweep in sweeps:
+            for rep in range(ua.reps):
+                outFile = f"{vcf_dir}/{sweep}/{rep}.multivcf"
+                dumpFile = f"{dumpfile_dir}/{sweep}/{rep}.dump"
+
+                # Grab those constants to feed to SLiM
+                if rep == 0:
+                    d_block = make_d_block(sweep, outFile, dumpFile, True)
+                else:
+                    d_block = make_d_block(sweep, outFile, dumpFile, False)
+
+                pool.apply_async(run_slim, args=(ua.slim_file, ua.slim_path, d_block))
+
+    # Cleanup
+    os.rmdir(dumpfile_dir)
+
+    # Log the constant params just in case, just use last one
+    with open(f"{work_dir}/slim_params.txt", "w") as paramsfile:
+        cleaned_block = [i.strip() for i in d_block.split("-d")]
+        paramsfile.writelines(cleaned_block)
+
+    logging.info(
+        f"Simulations finished, parameters saved to {work_dir}/slim_params.csv."
+    )
+
+
+if __name__ == "__main__":
+    main()
