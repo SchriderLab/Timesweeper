149e5414b928cf6b73389825949901f396c39232
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-03-14 19:04:22 -0400

    Input plotting now takes in new pickle format.

--- src/plotting/plot_input_data.py
+++ src/plotting/plot_input_data.py
@@ -1,15 +1,13 @@
-import random as rd
+import pickle
 from argparse import ArgumentParser
 from glob import glob
 
 import matplotlib as mpl
+import matplotlib.colors
+import matplotlib.pyplot as plt
 import numpy as np
 
 mpl.use("Agg")
-import multiprocessing as mp
-
-import matplotlib.colors
-import matplotlib.pyplot as plt
 
 
 def makeHeatmap(mat_type, data, plotTitle, axTitles, plotFileName):
@@ -27,14 +25,16 @@ def makeHeatmap(mat_type, data, plotTitle, axTitles, plotFileName):
     """
     plt.figure()
 
-    if mat_type == "afs":
-        fig, axes = plt.subplots(3, 1)
-    elif mat_type == "hfs":
-        fig, axes = plt.subplots(1, 3)
-
     minMin = np.amin(data) + 1e-6
     maxMax = np.amax(data)
-    normscheme = matplotlib.colors.Normalize(vmin=minMin, vmax=maxMax)
+
+    if mat_type == "afs":
+        fig, axes = plt.subplots(3, 1)
+        normscheme = matplotlib.colors.Normalize(vmin=minMin, vmax=maxMax)
+
+    elif mat_type == "hfs":
+        fig, axes = plt.subplots(1, 3)
+        normscheme = matplotlib.colors.Normalize(vmin=minMin, vmax=maxMax)
 
     for i in range(len(data)):
         heatmap = (axes[i].pcolor(data[i], cmap=plt.cm.Blues, norm=normscheme,),)[0]
@@ -57,12 +57,7 @@ def makeHeatmap(mat_type, data, plotTitle, axTitles, plotFileName):
     plt.clf()
 
 
-def loader(filename):
-    """Quick loader for multiprocessing."""
-    return np.load(filename)
-
-
-def readData(infiles):
+def readData(picklefile, data_type):
     """
     Loads in npy files and sorts based on sweep label.
 
@@ -73,18 +68,18 @@ def readData(infiles):
         Tuple[list[np.arr], list[np.arr], list[np.arr]]: Neut, hard, soft lists of arrays for processing.
     """
 
-    neutfiles = [aname for aname in infiles if "neut" in aname]
-    hardfiles = [aname for aname in infiles if "hard" in aname]
-    softfiles = [aname for aname in infiles if "soft" in aname]
+    pikl_dict = pickle.load(open(picklefile, "rb"))
+    neut_data = [
+        pikl_dict["neut"][rep][data_type.lower()] for rep in pikl_dict["neut"].keys()
+    ]
+    hard_data = [
+        pikl_dict["hard"][rep][data_type.lower()] for rep in pikl_dict["hard"].keys()
+    ]
+    soft_data = [
+        pikl_dict["soft"][rep][data_type.lower()] for rep in pikl_dict["soft"].keys()
+    ]
 
-    pool = mp.Pool(mp.cpu_count())
-    neut_loads = pool.map(loader, neutfiles)
-
-    hard_loads = pool.map(loader, hardfiles)
-
-    soft_loads = pool.map(loader, softfiles)
-
-    return neut_loads, hard_loads, soft_loads
+    return neut_data, hard_data, soft_data
 
 
 def getMeanMatrix(data):
@@ -100,21 +95,11 @@ def parse_ua():
 
     argparser.add_argument(
         "-i",
-        "--input",
-        dest="input",
+        "--input-pickle",
+        dest="input_pickle",
         metavar="INPUT DIR",
         type=str,
         help="Base directory containing <subdirs>/<afs/hfs>_center.npy. Will search through all sublevels of directories while globbing.",
-    )
-
-    argparser.add_argument(
-        "-m",
-        "--mat-type",
-        metavar="MAT TYPE",
-        choices=["AFS", "afs", "HFS", "hfs"],
-        dest="mat_type",
-        required=True,
-        help="Search and plot allele frequency data (--AFS) or haplotype frequency data (--HFS).",
     )
 
     argparser.add_argument(
@@ -145,72 +130,61 @@ def parse_ua():
 
 def main():
     ua = parse_ua()
-    base_dir = ua.input
-    mat_type = ua.mat_type.lower()
-    input_npys = glob(f"{base_dir}/**/npys/{mat_type}_center.npy", recursive=True)
-    schema_name = ua.schema_name
     plotDir = ua.output_dir
+    schema_name = ua.schema_name
 
-    base_filename = f"{plotDir}/{schema_name}_{mat_type}"
+    for mat_type in ["afs", "hfs"]:
+        base_filename = f"{plotDir}/{schema_name}_{mat_type}"
 
-    neut_list, hard_list, soft_list = readData(input_npys)
+        neut_list, hard_list, soft_list = readData(ua.input_pickle, mat_type)
 
-    neut_arr = np.stack(neut_list).transpose(0, 2, 1)
-    hard_arr = np.stack(hard_list).transpose(0, 2, 1)
-    soft_arr = np.stack(soft_list).transpose(0, 2, 1)
+        neut_arr = np.stack(neut_list).transpose(0, 2, 1)
+        hard_arr = np.stack(hard_list).transpose(0, 2, 1)
+        soft_arr = np.stack(soft_list).transpose(0, 2, 1)
 
-    if mat_type == "afs":
-        print(
-            "Shape of hard samples before mean (samples, snps, timepoints):",
-            hard_arr.shape,
-        )
-    elif mat_type == "hfs":
-        print(
-            "Shape of hard samples before mean (samples, haps, timepoints):",
-            hard_arr.shape,
-        )
+        if mat_type == "afs":
+            print(
+                "Shape of hard samples before mean (samples, snps, timepoints):",
+                hard_arr.shape,
+            )
+        elif mat_type == "hfs":
+            print(
+                "Shape of hard samples before mean (samples, haps, timepoints):",
+                hard_arr.shape,
+            )
 
-    mean_neut = getMeanMatrix(neut_arr)
-    mean_hard = getMeanMatrix(hard_arr)
-    mean_soft = getMeanMatrix(soft_arr)
+        mean_neut = getMeanMatrix(neut_arr)
+        mean_hard = getMeanMatrix(hard_arr)
+        mean_soft = getMeanMatrix(soft_arr)
 
-    print("Shape after mean:", mean_neut.shape)
-    print("Biggest value in hard (should be 1):", np.max(hard_arr))
+        print("Shape after mean:", mean_neut.shape)
+        print("Biggest value in hard (should be 1):", np.max(hard_arr))
 
-    makeHeatmap(
-        mat_type,
-        [mean_neut, mean_hard, mean_soft],
-        schema_name,
-        ["neut", "hard", "soft"],
-        base_filename + ".all.png",
-    )
-
-    if mat_type == "afs":
         makeHeatmap(
             mat_type,
-            [mean_neut[10:40, :], mean_hard[10:40, :], mean_soft[10:40, :]],
+            [mean_neut, mean_hard, mean_soft],
             schema_name,
             ["neut", "hard", "soft"],
-            base_filename + ".zoomed.png",
+            base_filename + ".all.png",
         )
 
-    elif mat_type == "hfs":
-        makeHeatmap(
-            mat_type,
-            [mean_neut[:20, :], mean_hard[:20, :], mean_soft[:20, :]],
-            schema_name,
-            ["neut", "hard", "soft"],
-            base_filename + ".zoomed.png",
-        )
+        if mat_type == "afs":
+            makeHeatmap(
+                mat_type,
+                [mean_neut[10:40, :], mean_hard[10:40, :], mean_soft[10:40, :]],
+                schema_name,
+                ["neut", "hard", "soft"],
+                base_filename + ".zoomed.png",
+            )
 
-    # for i in rd.sample(range(len(soft_arr)), 3):
-    #    makeHeatmap(
-    #        mat_type,
-    #        [neut_arr[i], hard_arr[i], soft_arr[i]],
-    #        schema_name + "singles",
-    #        ["Neut", "Hard", "Soft"],
-    #        f"{base_filename}_singles_{i}.zoomed.png",
-    #    )
+        elif mat_type == "hfs":
+            makeHeatmap(
+                mat_type,
+                [mean_neut[:20, :], mean_hard[:20, :], mean_soft[:20, :]],
+                schema_name,
+                ["neut", "hard", "soft"],
+                base_filename + ".zoomed.png",
+            )
 
 
 if __name__ == "__main__":
