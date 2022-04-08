04ef77fcc45dcefda892042ec74f6b36bd106f21
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-04-08 11:54:22 -0400

    Start of some testing.

---
+++ timesweeper/tests/test_fit.py
@@ -0,0 +1,22 @@
+from .. import frequency_increment_test as fit
+import numpy as np
+
+
+def test_getRescaledIncs():
+    incs = fit.getRescaledIncs(list(np.linspace(0, 1, 10)), list(range(0, 100, 10)))
+    assert incs == [
+        0.07905694150420947,
+        0.05976143046671968,
+        0.05270462766947298,
+        0.05000000000000002,
+        0.04999999999999997,
+        0.05270462766947295,
+        0.0597614304667197,
+    ]
+
+
+def test_fit():
+    assert (
+        fit.fit(list(np.linspace(0, 1, 10)), list(range(0, 100, 10)))[0]
+        == 14.864783097221553
+    )
