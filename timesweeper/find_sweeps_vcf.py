1879ed235be9198005cf7d85b78dd1b41889af83
Logan Whitehouse <lswhiteh@live.unc.edu>
2022-06-15 02:06:40 -0400

    Reimplement HFT in find_sweeps

--- timesweeper/find_sweeps_vcf.py
+++ timesweeper/find_sweeps_vcf.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 from .utils.frequency_increment_test import fit
 from .utils import snp_utils as su
 from .utils.gen_utils import read_config, write_fit, write_preds
+from .utils import hap_utils as hu
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
@@ -72,7 +73,7 @@ def run_aft_windows(snps, genos, samp_sizes, win_size, model):
     left_edges = []
     right_edges = []
     data = []
-    for center in tqdm(centers, desc="Predicting on aft windows"):
+    for center in tqdm(centers, desc="Predicting on AFT windows"):
         try:
             win_idxs = get_window_idxs(center, win_size)
             window = ts_aft[:, win_idxs]
@@ -82,6 +83,48 @@ def run_aft_windows(snps, genos, samp_sizes, win_size, model):
 
         except Exception as e:
             logger.warning(f"Center {snps[center]} raised error {e}")
+
+    probs = model.predict(np.stack(data))
+
+    results_dict = {}
+    for center, prob, l_e, r_e in zip(centers, probs, left_edges, right_edges):
+        results_dict[snps[center]] = (prob, l_e, r_e)
+
+    return results_dict
+
+
+def run_hft_windows(snps, haps, ploidy, samp_sizes, win_size, model):
+    """
+    Iterates through windows of MAF time-series matrix and predicts using NN.
+    Args:
+        snps (list[tup(chrom, pos,  mut)]): Tuples of information for each SNP. Contains mut only if benchmarking == True.
+        haps (np.arr): Haplotypes of all samples. 
+        samp_sizes (list[int]): Number of chromosomes sampled at each timepoint.
+        win_size (int): Number of SNPs to use for each prediction. Needs to match how NN was trained.
+        model (Keras.model): Keras model to use for prediction.
+    Returns:
+        dict: Prediction values in the form of dict[snps[center]]
+        np.arr: the central-most window, either based on mutation type or closest to half size of chrom.
+    """
+    results_dict = {}
+    buffer = math.floor(win_size / 2)
+    centers = range(buffer, len(snps) - buffer)
+    left_edges = []
+    right_edges = []
+    data = []
+    for center in tqdm(centers, desc="Predicting on HFS windows"):
+        try:
+            win_idxs = get_window_idxs(center, win_size)
+            window = np.swapaxes(haps[win_idxs, :], 0, 1)
+            str_window = hu.haps_to_strlist(window)
+            hfs = hu.getTSHapFreqs(str_window, [i* ploidy for i in samp_sizes])
+            data.append(hfs)
+            left_edges.append(snps[win_idxs[0]][1])
+            right_edges.append(snps[win_idxs[-1]][1])
+
+        except Exception as e:
+            logger.warning(f"Center {snps[center]} raised error {e}")
+            continue
 
     probs = model.predict(np.stack(data))
 
@@ -113,7 +156,6 @@ def run_fit_windows(snps, genos, samp_sizes, win_size, gens):
         results_dict[snps[idx]] = fit(list(ts_aft[:, idx]), gens)  # tval, pval
 
     return results_dict
-
 
 def get_window_idxs(center_idx, win_size):
     """
@@ -151,34 +193,31 @@ def load_nn(model_path, summary=False):
 def main(ua):
     if ua.config_format == "yaml":
         yaml_data = read_config(ua.yaml_file)
-        (work_dir, samp_sizes, outdir, aft_model) = (
+        (work_dir, samp_sizes, ploidy, outdir, aft_model) = (
             yaml_data["work dir"],
             yaml_data["sample sizes"],
+            yaml_data["ploidy"],
             ua.outdir,
             load_nn(ua.aft_model),
         )
 
         # If you're doing simple sims you probably aren't calculating years out
-        if "years sampled" in yaml_data:
-            years_sampled = yaml_data["years sampled"]
+        if "gens sampled" in yaml_data:
+            gens_sampled = yaml_data["gens sampled"]
         else:
-            years_sampled = None
+            gens_sampled = None
 
-        if "gen time" in yaml_data:
-            gen_time = yaml_data["gen time"]
-        else:
-            gen_time = None
 
     elif ua.config_format == "cli":
-        (samp_sizes, years_sampled, gen_time, work_dir, aft_model,) = (
+        (samp_sizes, gens_sampled, ploidy, work_dir, aft_model,) = (
             ua.samp_sizes,
-            ua.years_sampled,
-            ua.gen_time,
+            ua.gens_sampled,
+            ua.ploidy,
             ua.work_dir,
             load_nn(ua.aft_model),
         )
 
-    outdir = f"{work_dir}/timesweeper_output"
+    outdir = ua.outdir
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
@@ -201,21 +240,33 @@ def main(ua):
         except Exception as e:
             logger.error(f"Cannot process chunk {chunk_idx} using AFT due to {e}")
 
-    if years_sampled and gen_time:
+        # hft
+        if ua.hft_model:
+            try:
+                haps, snps = su.vcf_to_haps(chunk, ua.benchmark)
+                aft_predictions = run_hft_windows(
+                    snps, haps, ploidy, samp_sizes, win_size, load_nn(ua.hft_model)
+                )
+                write_preds(aft_predictions, f"{outdir}/hft_preds.csv", ua.benchmark)
+
+            except Exception as e:
+                logger.error(f"Cannot process chunk {chunk_idx} using HFT due to {e}")
+
+
+    if gens_sampled:
         vcf_iter = su.get_vcf_iter(ua.input_vcf, ua.benchmark)
         for chunk_idx, chunk in enumerate(vcf_iter):
             chunk = chunk[0]  # Why you gotta do me like that, skallel?
 
             # FIT
             # try:
-            gens = [i * gen_time for i in years_sampled]
             genos, snps = su.vcf_to_genos(chunk, ua.benchmark)
-            fit_predictions = run_fit_windows(snps, genos, samp_sizes, win_size, gens)
+            fit_predictions = run_fit_windows(snps, genos, samp_sizes, win_size, gens_sampled)
             write_fit(fit_predictions, f"{outdir}/fit_preds.csv", ua.benchmark)
             # except Exception as e:
             #    logger.error(f"Cannot process chunk {chunk_idx} using AFT due to {e}")
 
-        else:
-            logger.info(
-                "Cannot calculate FIT, years sampled and gen time not supplied."
-            )
+    else:
+        logger.info(
+            "Cannot calculate FIT, years sampled and gen time not supplied."
+        )
